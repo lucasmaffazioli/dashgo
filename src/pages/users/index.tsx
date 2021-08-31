5efcc3dd6d429c55f7fc5bd2ca1f34fb34f9665d
Lucas <lucas.maffazioli@gmail.com>
2021-08-31 11:26:34 +0200

    Pagination

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -15,6 +15,7 @@ import {
 } from '@chakra-ui/react'
 import { RiAddLine, RiPencilLine } from 'react-icons/ri'
 import Header from '../../components/Header'
+import { Pagination } from '../../components/Pagination'
 import Sidebar from '../../components/Sidebar'
 
 export default function UserList() {
@@ -167,6 +168,7 @@ export default function UserList() {
 							</Tr>
 						</Tbody>
 					</Table>
+					<Pagination></Pagination>
 				</Box>
 			</Flex>
 		</Box>
