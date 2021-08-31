1140895c1d6ac646ce9f286ceed5450ec9e27187
Lucas <lucas.maffazioli@gmail.com>
2021-08-31 11:37:33 +0200

    Create user

---
+++ src/pages/users/create.tsx
@@ -0,0 +1,82 @@
+import {
+	Box,
+	Button,
+	Divider,
+	Flex,
+	Heading,
+	HStack,
+	SimpleGrid,
+	VStack,
+} from '@chakra-ui/react'
+import { Input } from '../../components/Form/Input'
+import Header from '../../components/Header'
+import Sidebar from '../../components/Sidebar'
+
+export default function CreateUser() {
+	return (
+		<Box>
+			<Header />
+
+			<Flex
+				w='100%'
+				my='6'
+				maxWidth={1480}
+				mx='auto'
+				px='6'
+			>
+				<Sidebar />
+				<Box
+					flex='1'
+					borderRadius={8}
+					bgColor='gray.800'
+					p='8'
+				>
+					<Heading size='lg' fontWeight='normal'>
+						Create user
+					</Heading>
+
+					<Divider my='6' borderColor='gray.700'></Divider>
+
+					<VStack spacing='8'>
+						<SimpleGrid
+							minChildWidth='240px'
+							spacing='8'
+							w='100%'
+						>
+							<Input name='name' label='Name' />
+							<Input
+								name='password'
+								label='Password'
+								type='password'
+							/>
+						</SimpleGrid>
+						<SimpleGrid
+							minChildWidth='240px'
+							spacing='8'
+							w='100%'
+						>
+							<Input
+								name='password_confirmation'
+								label='Password confirmation'
+								type='password'
+							/>
+							<Input
+								name='email'
+								label='E-mail'
+								type='email'
+							/>
+						</SimpleGrid>
+					</VStack>
+					<Flex mt='8' justify='flex-end'>
+						<HStack spacing='4'>
+							<Button colorScheme='whiteAlpha'>
+								Cancel
+							</Button>
+							<Button colorScheme='pink'>Save</Button>
+						</HStack>
+					</Flex>
+				</Box>
+			</Flex>
+		</Box>
+	)
+}
