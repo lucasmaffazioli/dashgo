04b9f7c43c76fee633ab4e74b903aadecf8fafb1
Lucas <lucas.maffazioli@gmail.com>
2021-09-11 10:02:16 +0200

    Array with which pages to show

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -3,11 +3,82 @@ import {
 	Button,
 	HStack,
 	Stack,
+	Text,
 } from '@chakra-ui/react'
 import React from 'react'
 import { PaginationItem } from './PaginationItem'
+import { PaginationArrowItem } from './PaginationArrowItem'
 
-export function Pagination() {
+interface PaginationProps {
+	totalPages: number
+	currentPage: number
+}
+
+type Item = {
+	page?: number
+	type: ItemType
+}
+
+enum ItemType {
+	Page,
+	Dots,
+}
+
+export function Pagination({
+	totalPages,
+	currentPage,
+}: PaginationProps) {
+	let isFirst = false
+	let isLast = false
+
+	if (currentPage === 1) {
+		isFirst = true
+	}
+
+	if (currentPage === totalPages) {
+		isLast = true
+	}
+
+	// console.log('currentPage')
+	// console.log(currentPage)
+	// console.log('totalPages')
+	// console.log(totalPages)
+
+	function getItems() {
+		let items: Item[] = []
+
+		if (currentPage >= 3) {
+			items.push({
+				type: ItemType.Dots,
+			})
+		}
+
+		for (let i = 1; i <= totalPages; i++) {
+			const diff = currentPage - i
+			// console.log('Diff')
+			// console.log(diff)
+
+			if (diff <= 3 && diff >= -3) {
+				items.push({
+					page: i,
+					type: ItemType.Page,
+				})
+			} else {
+				if (i === totalPages) {
+					items.push({
+						type: ItemType.Dots,
+					})
+				}
+			}
+		}
+
+		// console.log('items')
+		// console.log(items)
+		return items
+	}
+
+	const lol = getItems()
+
 	return (
 		<Stack
 			direction={['column', 'row']}
@@ -22,6 +93,11 @@ export function Pagination() {
 			</Box>
 
 			<HStack spacing='2'>
+				<PaginationArrowItem
+					type='previous'
+					enabled={!isFirst}
+				/>
+
 				<PaginationItem
 					isCurrent
 					number={1}
@@ -31,6 +107,12 @@ export function Pagination() {
 				<PaginationItem number={4}></PaginationItem>
 				<PaginationItem number={5}></PaginationItem>
 				<PaginationItem number={6}></PaginationItem>
+				<Text px='2'>...</Text>
+
+				<PaginationArrowItem
+					type='next'
+					enabled={!isLast}
+				/>
 			</HStack>
 		</Stack>
 	)
